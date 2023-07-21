97851d84bc0ce7ae9cd8e439f1828832da9c274d
LimpidCrypto <info@limpidcrypto.de>
2023-07-22 01:14:06 +0200

    add XRPLWebsocketException

---
+++ src/asynch/clients/websocket/exceptions.rs
@@ -0,0 +1,16 @@
+use thiserror_no_std::Error;
+use tokio_tungstenite::tungstenite;
+
+#[derive(Debug, Error)]
+pub enum XRPLWebsocketException {
+    #[cfg(feature = "std")]
+    #[error("Tungstenite: `{0:?}`")]
+    Tungstenite(tungstenite::Error)
+}
+
+#[cfg(feature = "std")]
+impl From<tungstenite::Error> for XRPLWebsocketException {
+    fn from(value: tungstenite::Error) -> Self {
+        Self::Tungstenite(value)
+    }
+}
